455bb6fb292baab6a6365a0b0eebedcecfa43109
Erik Madsen <beatmadsen@gmail.com>
2015-09-12 17:19:42 +0200

    Initial Commit

---
+++ LetterChat/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LetterChat
+//
+//  Created by Erik Madsen on 12/09/2015.
+//  Copyright (c) 2015 Erik Madsen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
