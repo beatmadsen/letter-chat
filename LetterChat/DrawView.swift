6de1a277dc8f1a655500c8558e2d204ed814f067
Erik Madsen <beatmadsen@gmail.com>
2015-09-17 01:14:41 +0200

    EM: New drawing algorithm, WIP

--- LetterChat/DrawView.swift
+++ LetterChat/DrawView.swift
@@ -9,77 +9,108 @@
 import UIKit
 
 class DrawView: UIView {
-    
-    private var linesByColour: [UIColor:[Line]] = [:]
-    private var lastPoint: CGPoint! // implicitly unwrapped optional, i.e. it will behave like a normal var, but it may be nil
-    
+
     var colour: UIColor = UIColor.blackColor()
+    
+    private var incrementalImage:UIImage?
+    
+    
+    private var path: UIBezierPath = UIBezierPath()
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        linesByColour.updateValue([], forKey: UIColor.redColor())
-        linesByColour.updateValue([], forKey: UIColor.blackColor())
+        self.multipleTouchEnabled = false
+        self.backgroundColor = UIColor.whiteColor()
         
-    }
+        path.lineWidth = 2.0
+        
+        println(self.bounds.size)
+}
     
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         if let touch = touches.first as? UITouch {
-            lastPoint = touch.locationInView(self)
+            var p = touch.locationInView(self)
+            path.moveToPoint(p)
         }
-        super.touchesBegan(touches, withEvent: event)
     }
 
     
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         if let touch = touches.first as? UITouch {
             let newPoint = touch.locationInView(self)
-            addPoint(newPoint)
-            lastPoint = newPoint
+            path.addLineToPoint(newPoint)
+            self.setNeedsDisplay()
         }
-        super.touchesBegan(touches, withEvent: event)
-        self.setNeedsDisplay()
 
     }
     
     
-    override func drawRect(rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
-        
-        CGContextBeginPath(context)
-        CGContextSetLineWidth(context, 5)
-        CGContextSetLineCap(context, kCGLineCapRound)
-        
-        for (colour, lines) in linesByColour {
-            
-            CGContextSetStrokeColorWithColor(context, colour.CGColor)
-     
-            for line in lines {
-                CGContextMoveToPoint(context, line.start.x, line.start.y)
-                CGContextAddLineToPoint(context, line.end.x, line.end.y)
-            }
-            CGContextStrokePath(context)
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if incrementalImage == nil {
+            drawInitialBitmap()
         }
+        
+        if let touch = touches.first as? UITouch {
+            let newPoint = touch.locationInView(self)
+            path.addLineToPoint(newPoint)
+            drawBitmap()
+            path.removeAllPoints()
+            self.setNeedsDisplay()
+        }
+    }
+    
+    
+    override func touchesCancelled(touches: Set<NSObject>!, withEvent event: UIEvent!) {
+        touchesEnded(touches, withEvent: event)
+    }
+    
+    
+    override func drawRect(rect: CGRect) {
+        incrementalImage?.drawInRect(rect)
+        colour.setStroke()
+        path.stroke()
+    }
+    
+    
+    private func doWithinGraphicsContext(action: () -> Void) {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, 0.0)
+        action()
+        UIGraphicsEndImageContext()
+    }
+    
+    
+    private func drawInitialBitmap() {
+        
+        doWithinGraphicsContext({
+            () -> Void in
+            var rectPath = UIBezierPath(rect: self.bounds)
+            UIColor.whiteColor().setFill()
+            rectPath.fill()
+            
+            self.incrementalImage = UIGraphicsGetImageFromCurrentImageContext()
+        })
+    }
+    
+    
+    private func drawBitmap() {
+
+        doWithinGraphicsContext({
+            () -> Void in
+            self.incrementalImage?.drawAtPoint(CGPointZero)
+            self.colour.setStroke()
+            self.path.stroke()
+            
+            self.incrementalImage = UIGraphicsGetImageFromCurrentImageContext()
+        })
     }
     
     
     func clearLines() {
         
-        for key in linesByColour.keys {
-            linesByColour[key]!.removeAll(keepCapacity: true)
-        }
+        path.removeAllPoints()
         
         self.setNeedsDisplay()
     }
-    
-    
-    private func addPoint(point: CGPoint) {
-        linesByColour[colour]?.append(Line(start: lastPoint, end: point))
-    }
-}
-
-struct Line {
-    let start: CGPoint
-    let end: CGPoint
 }
