a0fbd7af36729edbbcfab9b591e97934bf4d4479
Erik Madsen <beatmadsen@gmail.com>
2015-09-18 23:23:05 +0200

    EM: Smoother lines

--- LetterChat/DrawView.swift
+++ LetterChat/DrawView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class DrawView: UIView {
-
+    
     var colour: UIColor = UIColor.blackColor()
     
     private var incrementalImage:UIImage?
@@ -17,35 +17,49 @@ class DrawView: UIView {
     
     private var path: UIBezierPath = UIBezierPath()
     
+    private var rotator: PointRotator! = nil
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
+        self.rotator = PointRotator( callback: {
+            points in
+            
+            self.path.moveToPoint(points[0])
+            self.path.addCurveToPoint(points[3], controlPoint1: points[1], controlPoint2: points[2])
+            self.setNeedsDisplay()
+            
+            return CGPointMake(0,0) // TODO - change signature
+        })
         
         self.multipleTouchEnabled = false
         self.backgroundColor = UIColor.whiteColor()
         
         path.lineWidth = 2.0
         
-        print(self.bounds.size)
-}
+        
+    }
     
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch = touches.first
+        
         let p = touch?.locationInView(self)
         
         if let point = p {
-            path.moveToPoint(point)
+            rotator.append(point)
         }
+        
     }
-
+    
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch = touches.first
+        
         let p = touch?.locationInView(self)
         
         if let point = p {
-            path.addLineToPoint(point)
-            self.setNeedsDisplay()
+            rotator.append(point)
         }
     }
     
@@ -54,15 +68,11 @@ class DrawView: UIView {
         if incrementalImage == nil {
             drawInitialBitmap()
         }
-        let touch = touches.first
-        let p = touch?.locationInView(self)
         
-        if let point = p {
-            path.addLineToPoint(point)
-            drawBitmap()
-            path.removeAllPoints()
-            self.setNeedsDisplay()
-        }
+        drawBitmap()
+        self.setNeedsDisplay()
+        rotator.reset()
+        path.removeAllPoints()
     }
     
     
@@ -101,7 +111,7 @@ class DrawView: UIView {
     
     
     private func drawBitmap() {
-
+        
         doWithinGraphicsContext({
             () -> Void in
             self.incrementalImage?.drawAtPoint(CGPointZero)
@@ -117,6 +127,39 @@ class DrawView: UIView {
         
         path.removeAllPoints()
         
+        drawInitialBitmap()
+        
         self.setNeedsDisplay()
+    }
+}
+
+
+
+class PointRotator {
+    private let onRotation: [CGPoint] -> CGPoint
+    private var points: [CGPoint] = []
+    
+    init(callback: [CGPoint] -> CGPoint){
+        self.onRotation = callback
+    }
+    
+    func reset() {
+        points.removeAll(keepCapacity: true)
+    }
+    
+    func append(newElement: CGPoint){
+        points.append(newElement)
+        
+        if points.count == 5 {
+            
+            let x = (points[2].x + points[4].x) / 2.0
+            let y = (points[2].y + points[4].y) / 2.0
+            
+            points[3] = CGPointMake(x, y)
+            
+            onRotation(points)
+            
+            points.removeFirst(3)
+        }
     }
 }
